6a299c062d8ae981b4b73bb4a4076039b6e5a3bb
Ramesh Guddala <ramesh.guddala@excelicare.com>
2018-12-20 18:05:53 +0530

    Initial Commit

---
+++ PlanetsList_Ramesh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlanetsList_Ramesh
+//
+//  Created by Ramesh Guddala on 20/12/18.
+//  Copyright © 2018 Ramesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
